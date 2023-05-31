bd4e0923d5fadb4ffc463a6338fa27d74959c557
Lindsay Warr <iamalion@gmail.com>
2023-05-31 16:07:16 -0700

    Change js to be more readable

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-window.onload = function () {
-    let form = document.querySelector("form");
-    form.onsubmit = function (event) {
+// window.onload = function () {
+   // let form = document.querySelector("form");
+    // form.onsubmit = function (event) {
+function getAndSetMadLibValues () {
+
+
         const person1Input = document.getElementById("person1Input").value;
         const person2Input = document.getElementById("person2Input").value;
         const animalInput= document.getElementById("animalInput").value;
@@ -8,18 +11,28 @@ window.onload = function () {
         const verbInput = document.getElementById("verbInput").value;
         const nounInput = document.getElementById("nounInput").value;
 
-        document.querySelector("span#person1a").innerText = "person1";
-        document.querySelector("span#person1b").innerText = "person 1";
-        document.querySelector("span#person1c").innerText = "person 1";
-        document.querySelector("span#person2a").innerText = "person 2";
-        document.querySelector("span#person2b").innerText = "person 2";
-        document.querySelector("span#animal").innerText = "an animal";
-        document.querySelector("span#verb").innerText = "verb";
-        document.querySelector("span#noun").innerText = "noun";
-        document.querySelector("span#exclamation").innerText = "exclamation";
+        document.querySelector("span#person1a").innerText = person1Input;
+        document.querySelector("span#person1b").innerText = person1Input;
+        document.querySelector("span#person1c").innerText = person1Input;
+        document.querySelector("span#person2a").innerText = person2Input;
+        document.querySelector("span#person2b").innerText = person2Input;
+        document.querySelector("span#animal").innerText = animalInput;
+        document.querySelector("span#verb").innerText = verbInput;
+        document.querySelector("span#noun").innerText = nounInput;
+        document.querySelector("span#exclamation").innerText = exclamationInput;
 
         document.querySelector("div#story").removeAttribute("class")
         
         event.preventDefault();
     }
+function setFormSubmissionEventHandler () {
+    let form = document.querySelector("form");
+    form.onsubmit = function (event) {
+        event.preventDefault();
+        getAndSetMadLibValues();
+        document.querySelector("div#story").removeAttribute("class");
+    }    
+}
+window.onload = function () {
+    setFormSubmissionEventHandler();
 }
