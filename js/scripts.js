2c1d0e3163103e8a399806451a00b0aa641b26af
Lindsay Warr <iamalion@gmail.com>
2023-05-31 16:23:44 -0700

    Fix bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,26 @@
-// window.onload = function () {
-   // let form = document.querySelector("form");
-    // form.onsubmit = function (event) {
-function getAndSetMadLibValues () {
-
-
-        const person1Input = document.getElementById("person1Input").value;
-        const person2Input = document.getElementById("person2Input").value;
-        const animalInput= document.getElementById("animalInput").value;
-        const exclamationInput = document.getElementById("exclamationInput").value;
-        const verbInput = document.getElementById("verbInput").value;
-        const nounInput = document.getElementById("nounInput").value;
-
-        document.querySelector("span#person1a").innerText = person1Input;
-        document.querySelector("span#person1b").innerText = person1Input;
-        document.querySelector("span#person1c").innerText = person1Input;
-        document.querySelector("span#person2a").innerText = person2Input;
-        document.querySelector("span#person2b").innerText = person2Input;
-        document.querySelector("span#animal").innerText = animalInput;
-        document.querySelector("span#verb").innerText = verbInput;
-        document.querySelector("span#noun").innerText = nounInput;
-        document.querySelector("span#exclamation").innerText = exclamationInput;
-
-        document.querySelector("div#story").removeAttribute("class")
-        
-        event.preventDefault();
-    }
-function setFormSubmissionEventHandler () {
+window.onload = function() {
     let form = document.querySelector("form");
-    form.onsubmit = function (event) {
-        event.preventDefault();
-        getAndSetMadLibValues();
-        document.querySelector("div#story").removeAttribute("class");
-    }    
-}
-window.onload = function () {
-    setFormSubmissionEventHandler();
-}
+    form.onsubmit = function(event) {
+      event.preventDefault();
+      const person1Input = document.getElementById("person1Input").value;
+      const person2Input = document.getElementById("person2Input").value;
+      const animalInput= document.getElementById("animalInput").value;
+      const exclamationInput = document.getElementById("exclamationInput").value;
+      const verbInput = document.getElementById("verbInput").value;
+      // Here's the first log we added.
+      console.log("verbInput = " + verbInput);
+      const nounInput = document.getElementById("nounInput").value;
+  
+      document.querySelector("span#person1a").innerText = person1Input;
+      document.querySelector("span#person1b").innerText = person1Input;
+      document.querySelector("span#person1c").innerText = person1Input;
+      document.querySelector("span#person2a").innerText = person2Input;
+      document.querySelector("span#person2b").innerText = person2Input;
+      document.querySelector("span#animal").innerText = animalInput;
+      document.querySelector("span#verb").innerText = verbInput;
+      document.querySelector("span#noun").innerText = nounInput;
+      document.querySelector("span#exclamation").innerText = exclamationInput;
+  
+      document.querySelector("div#story").removeAttribute("class");
+    };
+  };
